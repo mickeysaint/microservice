2197faf39d235905eb7f817694765eebbf289ecc
smz <mickeysaint@163.com>
2020-01-21 09:09:10 +0800

    系统配置等

--- service/baseservice/src/main/java/com/xyz/ms/service/baseservice/controller/SysconfigController.java
+++ service/baseservice/src/main/java/com/xyz/ms/service/baseservice/controller/SysconfigController.java
@@ -1,46 +1,32 @@
 package com.xyz.ms.service.baseservice.controller;
 
+import com.xyz.base.common.Page;
 import com.xyz.base.common.ResultBean;
 import com.xyz.base.exception.BusinessException;
 import com.xyz.base.po.base.SysConfigPo;
 import com.xyz.base.util.AssertUtils;
 import com.xyz.ms.service.baseservice.service.SysConfigService;
 import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/sysconfig")
 public class SysconfigController {
 
+    Logger logger = LoggerFactory.getLogger(SysconfigController.class);
+
     @Autowired
     private SysConfigService sysConfigService;
-
-    @RequestMapping("/add")
-    public ResultBean<Void> add(SysConfigPo sysconfig) {
-        ResultBean<Void> ret = new ResultBean<>();
-
-        try {
-            AssertUtils.isTrue(sysconfig != null, "没有接收到待保存的数据。");
-            AssertUtils.isTrue(StringUtils.isNotEmpty(sysconfig.getConfigKey()), "配置项编码不能为空。");
-            AssertUtils.isTrue(StringUtils.isNotEmpty(sysconfig.getConfigName()), "配置项名称不能为空。");
-            AssertUtils.isTrue(sysConfigService.exists(sysconfig.getConfigKey()), "该字典类型已经存在。");
-
-            sysConfigService.save(sysconfig);
-        } catch(BusinessException e) {
-            ret.setSuccess(false);
-            ret.setMessage(e.getMessage());
-        } catch(Exception e) {
-            ret.setSuccess(false);
-            ret.setMessage("操作失败");
-        }
-
-        return ret;
-    }
 
     @RequestMapping("/getById")
     public ResultBean<SysConfigPo> getById(Long id) {
@@ -60,36 +46,57 @@ public class SysconfigController {
         return ret;
     }
 
-    @RequestMapping("/update")
-    public ResultBean<Void> update(SysConfigPo sysConfigPo) {
-        ResultBean<Void> ret = new ResultBean<>();
-        try {
-            AssertUtils.isTrue(sysConfigPo != null, "没有接收到待保存的数据。");
-            AssertUtils.isTrue(StringUtils.isNotEmpty(sysConfigPo.getConfigKey()), "配置项编码不能为空。");
-            AssertUtils.isTrue(StringUtils.isNotEmpty(sysConfigPo.getConfigName()), "配置项名称不能为空。");
-
-            SysConfigPo another = sysConfigService.findByConfigKey(sysConfigPo.getConfigKey());
-            if (another != null && !another.getId().equals(sysConfigPo.getId())) {
-                throw new BusinessException("配置项编码重复。");
-            }
-
-            sysConfigService.update(sysConfigPo);
-        } catch(BusinessException e) {
-            ret.setSuccess(false);
-            ret.setMessage(e.getMessage());
-        } catch(Exception e) {
-            ret.setSuccess(false);
-            ret.setMessage("操作失败");
-        }
-
-        return ret;
-    }
-
     @RequestMapping("/delete")
     public ResultBean<Void> delete(Long id) {
         SysConfigPo sysconfig = sysConfigService.findById(id);
         sysConfigService.delete(sysconfig);
         ResultBean<Void> ret = new ResultBean<Void>();
+        return ret;
+    }
+
+    @RequestMapping("/getListData")
+    public ResultBean<Page<SysConfigPo>> getListData(@RequestBody Map params, HttpServletRequest request) {
+        ResultBean<Page<SysConfigPo>> ret = new ResultBean<>();
+        Page<SysConfigPo> sysConfigs = sysConfigService.getListData(params);
+        ret.setData(sysConfigs);
+        return ret;
+    }
+
+    @RequestMapping("/save")
+    public ResultBean<Void> save(@RequestBody SysConfigPo sysConfigPo) {
+        ResultBean<Void> ret = new ResultBean<>();
+        try {
+            AssertUtils.isTrue(sysConfigPo != null, "没有接收到数据。");
+            AssertUtils.isTrue(StringUtils.isNotEmpty(sysConfigPo.getConfigKey()), "配置项代码不能为空。");
+            AssertUtils.isTrue(StringUtils.isNotEmpty(sysConfigPo.getConfigName()), "配置项名称不能为空。");
+            AssertUtils.isTrue(StringUtils.isNotEmpty(sysConfigPo.getConfigValue()), "配置项值不能为空。");
+
+            AssertUtils.isTrue(
+                    (
+                            (sysConfigPo.getId() != null)
+                                    || (sysConfigPo.getId() == null && !sysConfigService.exists(sysConfigPo.getConfigKey()))
+                    ),
+                    "该配置项已经存在。");
+
+            if (sysConfigPo.getId() != null) {
+                SysConfigPo editPo = sysConfigService.findById(sysConfigPo.getId());
+                editPo.setConfigName(sysConfigPo.getConfigName());
+                editPo.setConfigValue(sysConfigPo.getConfigValue());
+                editPo.setRemark(sysConfigPo.getRemark());
+                sysConfigService.update(editPo);
+            } else {
+                sysConfigService.update(sysConfigPo);
+            }
+        } catch(BusinessException e) {
+            logger.error("保存组织结构出错", e);
+            ret.setSuccess(false);
+            ret.setMessage(e.getMessage());
+        } catch(Exception e) {
+            logger.error("保存组织结构出错", e);
+            ret.setSuccess(false);
+            ret.setMessage("操作失败");
+        }
+
         return ret;
     }
 }
